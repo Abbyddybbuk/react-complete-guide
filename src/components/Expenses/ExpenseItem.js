7d1b35623004abc0ed5d4f10cf0d910dd3ab8569
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-01-17 18:53:55 +0000

    UserInput New Syntax/Collective User Input

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
-<<<<<<< HEAD
+
 import React, { useState } from 'react';
 
 const ExpenseItem = (props) => {
@@ -12,26 +12,14 @@ const ExpenseItem = (props) => {
         setTitle('Updated!');
         console.log(title);
     };
-=======
-
-const ExpenseItem = (props) => {
-
->>>>>>> 570d833a648032b2c3df0a0f3c606d3fa004a893
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-<<<<<<< HEAD
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-=======
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
->>>>>>> 570d833a648032b2c3df0a0f3c606d3fa004a893
         </Card>
     );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,40 @@ import React, { useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+    //Instead of setting state for individual fields, you can actually
+    // set state for the form fields together as shown below
+    const [userInput, setUserInput] = useState({
+       enteredTitle: '',
+       enteredAmount: '',
+       enteredDate: '',
+    });
    
     const titleChangeHandler = (event) => {
-       setEnteredTitle(event.target.value);       
+        setUserInput({
+            ...userInput,//This means that with change of value of Entered Title you do not want to override entered
+                         // values of Amount and Date
+            enteredTitle: event.target.value,
+        });
+    //    setEnteredTitle(event.target.value);       
     };
 
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        });        
+        // setEnteredAmount(event.target.value);
     }
 
     const amountDateHandler = (event) => {
-        setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        });         
+        // setEnteredDate(event.target.value);
     }
 
     return <form>
