b834a8b6fcd50f61d3aa0a7143ea97b65884c0a0
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-01-18 18:06:41 +0000

    Form Submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,43 +3,64 @@ import React, { useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
     //Instead of setting state for individual fields, you can actually
     // set state for the form fields together as shown below
-    const [userInput, setUserInput] = useState({
-       enteredTitle: '',
-       enteredAmount: '',
-       enteredDate: '',
-    });
+    // const [userInput, setUserInput] = useState({
+    //    enteredTitle: '',
+    //    enteredAmount: '',
+    //    enteredDate: '',
+    // });
    
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,//This means that with change of value of Entered Title you do not want to override entered
-                         // values of Amount and Date
-            enteredTitle: event.target.value,
-        });
-    //    setEnteredTitle(event.target.value);       
+        // setUserInput({
+        //     ...userInput,//This means that with change of value of Entered Title you do not want to override entered
+        //                  // values of Amount and Date
+        //     enteredTitle: event.target.value,
+        // });
+        //You should not use the syntax above as the above syntax may cause inconsistency state issues
+        //In reactjs, state is not changed immediately when you are setting one instead it is set to change later
+        //Therefore, above syntax (at line ...userInput) may be working with older state.
+        // setUserInput((prevState) => {
+        //   return { ...prevState, enteredTitle: event.target.value};
+        // });
+
+       setEnteredTitle(event.target.value);       
     };
 
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        });        
-        // setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });        
+        setEnteredAmount(event.target.value);
     }
 
     const amountDateHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        });         
-        // setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });         
+        setEnteredDate(event.target.value);
     }
 
-    return <form>
+    const submitHandler = (event) => {
+      event.preventDefault();
+
+      const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+      };
+
+      console.log(expenseData);
+    };
+
+    // With Button as type "Submit"....form is considered as a whole and form provides onSubmit event
+    return <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
