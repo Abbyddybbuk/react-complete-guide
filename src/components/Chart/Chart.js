78ae8ff7f646edb85c99128ab2fc7768f340d98c
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-01-29 00:29:02 +0000

    Addition of Bar Chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,16 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);//spread operator is being used as dataPointValues is an array but Math.max works on list of numbers
+
     return (
         <div className='chart'>
             {props.dataPoints.map((dataPoint) => (
                 <ChartBar
                     key={dataPoint.label}
                     value={dataPoint.value}
-                    maxValue={null}
+                    maxValue={totalMaximum}
                     label={dataPoint.label}
                 />
             ))}
